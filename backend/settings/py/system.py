9ce32f7bb0b0b55176287160d5e684bd9fc424a8
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-24 22:38:55 +0800

    before initial setup

--- backend/settings/py/system.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-now = datetime.now()
-formatted_date = now.strftime("%d/%m/%Y-%H:%M:%S")
-formatted_date_DMY = now.strftime("%d-/%m-/%Y")
-
-
-class Paths:
-    LOGS = f"logs/{formatted_date}-log.log"
